6af00ca133a482f55f9a4da678fde8944979779c
gokberkkoksoy <gokberkkoksoy@icloud.com>
2021-02-28 18:48:22 +0300

    MapKit initialized with capital cities.

---
+++ Capital Cities/Capital.swift
@@ -0,0 +1,21 @@
+//
+//  Capital.swift
+//  Capital Cities
+//
+//  Created by Gökberk Köksoy on 28.02.2021.
+//
+
+import UIKit
+import MapKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}
