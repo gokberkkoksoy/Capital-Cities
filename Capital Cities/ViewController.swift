9ef357fdbc0ed9d55e548ce9ba7804906437bc3e
gokberkkoksoy <gokberkkoksoy@icloud.com>
2021-02-28 18:29:03 +0300

    Initial Commit

---
+++ Capital Cities/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Capital Cities
+//
+//  Created by Gökberk Köksoy on 28.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
